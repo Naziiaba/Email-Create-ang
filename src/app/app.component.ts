d18cb436363e817bb8b6670aedcab7416a2116b0
Zia pmr <gernersnc@gmail.com>
2020-03-11 14:38:42 -0500

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+
+/**Component DECORATOR and METADATA
+ * 
+ * selector: this will act as an element tag to use this component.
+ * templateUrl: is a VIEW for my component.
+ * styleUrl: css styling
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+/**
+ * Data binding
+ * 1. interpolation - component class to the view
+ * 2. Event - from view to component class data binding
+ */
 export class AppComponent {
   title = 'Email-Create-ang';
+  pageContent: string;
+
+  constructor() {
+    this.pageContent = 'The Content goes here';
+  }
+
+  /**
+   * Lifecycle Hooks
+   */
 }
