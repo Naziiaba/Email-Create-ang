9e0f60a1ac3331512674c77e4553ee5018bea144
Zia pmr <gernersnc@gmail.com>
2020-03-12 16:24:30 -0500

    a new component - mailbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MailboxComponent } from './mailbox/mailbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MailboxComponent
   ],
   imports: [
     BrowserModule,
