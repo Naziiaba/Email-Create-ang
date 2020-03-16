5d134b76c6f9bf41d43c7917fb87aaa61c1176ba
Zia pmr <gernersnc@gmail.com>
2020-03-16 11:30:20 -0500

    Input and Output decorators

--- src/app/mailbox/left-nav/left-nav.component.ts
+++ src/app/mailbox/left-nav/left-nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-left-nav',
@@ -7,11 +8,31 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 
 export class LeftNavComponent implements OnInit {
+
+  /**
+   * Parent to child comm: To send data from parent to child we can use Input decorator
+   * Example:
+   * <app-mailbox> (navLinks)
+   *    <app-left-nav [navData] = "navLinks"><app-left-nav>
+   * </app-mailbox>
+   */
   @Input() navData: Array<string>;
 
+  /**
+   * Child --> Paretn comm:
+   * <app-mailbox> (navLinks)
+   *    <app-left-nav [navData] = "navLinks" (menuSelectionChange)
+   * = "handleFunction()"><app-left-nav>
+   * </app-mailbox>
+   */
+  @Output() menuSelectionChange = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onLinkClick(linkIndex: number) {
+    this.menuSelectionChange.emit(linkIndex);
   }
 
 }
--- src/app/mailbox/mailbox.component.ts
+++ src/app/mailbox/mailbox.component.ts
@@ -12,4 +12,7 @@ export class MailboxComponent {
     'Sent Items'
   ];
 
+  onSelectionChange(event) {
+
+  }
 }
