814cdf2e3d0773b47d3b71669a2425bf2fdf1dab
Zia pmr <gernersnc@gmail.com>
2020-03-16 17:26:02 -0500

    methods like ngOnContentInit, ngDoChecked etc added

--- src/app/mailbox/left-nav/left-nav.component.ts
+++ src/app/mailbox/left-nav/left-nav.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-left-nav',
   templateUrl: './left-nav.component.html',
-  styleUrls: ['./left-nav.component.css']
+  styleUrls: ['./left-nav.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 
 export class LeftNavComponent implements OnInit {
-
   /**
    * Parent to child comm: To send data from parent to child we can use Input decorator
    * Example:
@@ -31,23 +31,74 @@ export class LeftNavComponent implements OnInit {
    * and use that to bind in views => Input.
    * Parent to child -Listener => action is happening in parent and child should
    *  listen it => use Lifecycle Hook called - ngOnChanges
-   * Parent to child - Listener => take reference of child class in parent 
+   * Parent to child - Listener => take reference of child class in parent
    * component and access required methods - ViewChild.
    * Child to Parent => child has some action, parent will get notified
-   * using an event => Output decorator
+   * using an event => Output decorator.
+   * Communicating independent/unrelational components: by creating Service
+   * (rx concepts) to be added in the service session.
+   */
+
+  /**
+   * Constructor - executes first
+   * It wont have data provided by parent.
+   * Input properties will be undefined in this phase.
    */
   constructor() { }
 
+  /**
+   * This is first method to get called in lifecycle of component.
+   * It will again get called whenever parent is changing value of data binding
+   * to Input property of component.
+   */
+
   ngOnChanges(changes) {
     if (changes.navData) {
-      //this.processData();
+      // this.processData();
     }
   }
+
+  /**
+   * Second method in lifecycle hook
+   * So write all the execution of logic needed at the beginning of component load.
+   */
   ngOnInit() {
     /**
      * process the input data and modify it according to the business requirement.
      */
-    this.navData.push('middle');
+  }
+  /**
+   * it detects changes that angular cannot.
+   */
+  // ngDoCheck() {
+  // }
+  
+  
+  /**
+   * All external contents are checked for data binding and angular processing.
+   */
+  ngOnContentInit() {
+  }
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngAfterContentChecked() {
+    
+  }
+   ngOnViewInit() {
+
+   }
+
+   ngOnViewChecked() {
+   }
+
+  /**
+   * The last method to get called in component lifecycle.
+   * Unsubscribe any events attached to observables.
+   * This is required for the performance.
+   */
+
+  ngOnDestroy() {
+
   }
 
   onLinkClick(linkIndex: number) {
