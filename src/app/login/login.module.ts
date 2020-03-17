27c17781ec857a15510c75d77ae679209a44c326
Zia pmr <gernersnc@gmail.com>
2020-03-17 13:30:18 -0500

    new login.module.ts

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from './login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LeftNavComponent,
+    MailboxComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
