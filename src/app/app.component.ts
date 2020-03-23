e14f3b7e35132fb6710d6d0dde63f884f4801dc8
Zia pmr <gernersnc@gmail.com>
2020-03-23 15:14:23 -0500

    appTooltip Directives and implementation, css updates.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 
 /**Component DECORATOR and METADATA
@@ -9,7 +9,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 /**
  * Data binding
--- src/app/shared/tooltip.directive.ts
+++ src/app/shared/tooltip.directive.ts
@@ -1,11 +1,25 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
     selector: '[appTooltip]'
 })
 
 export class AppTooltipDirective {
+    @Input() appTooltip;
+    currentElement;
+    tooltipElem;
     constructor(private elementRef: ElementRef) {
-        console.log(elementRef.nativeElement);
+        elementRef.nativeElement.style.color = 'red';
+        this.currentElement = elementRef.nativeElement;
+    }
+
+    @HostListener('mouseenter', ['$event.target']) onMouseEnter(event) {
+        this.tooltipElem = document.createElement('span');
+        this.tooltipElem.setAttribute('class', 'tooltip');
+        this.tooltipElem.innerHTML = this.appTooltip;
+        this.elementRef.nativeElement.appendChild(this.tooltipElem);
+    }
+    @HostListener('mouseleave', ['$event.target']) onMouseLeave(event) {
+        this.elementRef.nativeElement.removeChild(this.tooltipElem);
     }
 }
